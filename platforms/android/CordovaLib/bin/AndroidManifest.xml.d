8946127ce61cbf09eea1bac915097a53d0cb16a8
Akhilesh Gupta <agupta@salesforce.com>
2014-10-09 01:56:32 -0700

    updating android platform dependencies

--- platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/agupta/temp/Offline Demo/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
-/Users/agupta/temp/Offline Demo/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/agupta/temp/Offline Demo/platforms/android/CordovaLib/bin/AndroidManifest.xml \
